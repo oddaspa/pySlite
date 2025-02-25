044b9a2fad2e52fa6a2ba7b9e3e7151838336b71
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2025-02-25 15:28:32 +0100

    git fix error note

--- src/pyslite/utils/note.py
+++ src/pyslite/utils/note.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List
+from pydantic import BaseModel, validator
+from typing import Optional
+
 from enum import Enum
-from pydantic import BaseModel
 
-
-class ReviewState(str, Enum):
-    Verified = "Verified"
-    Outdated = "Outdated"
-    VerificationRequested = "VerificationRequested"
+class ReviewState(Enum):
+  Verified = "Verified"
+  Outdated = "Outdated"
+  VerificationRequested = "VerificationRequested"
 
 
 class Note(BaseModel):
@@ -16,27 +17,20 @@ class Note(BaseModel):
     parentNoteId: str
     title: str
     id: str
-    content: Optional[str] = ""  # content can be missing
+    content: Optional[str] = None  # content can be missing
     reviewState: Optional[ReviewState] = None
     columns: Optional[List[str]] = None
     attributes: Optional[List[str]] = None
-
+    
 
     def show(self):
-        try:
-            from IPython import display, get_ipython
-            get_ipython()  # Check if running in IPython
-            if self.content:
-                content = f"# {self.title}\n" + self.content
-                display.display(display.Markdown(content))
-            else:
-                return "No Content"
-        except ImportError:
-            print(f"Note Title: {self.title}")
-            if self.content:
-                print(f"Note Content:\n{self.content}")
-            else:
-                print("No Content")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
+      try:
+        from IPython import display, get_ipython
+        get_ipython()
+        if self.content:
+          display_content = f"#{self.title}\n" + self.content
+          display.display(display.Markdown(display_content))
+        
+      except:
+        print(self.title)
+        print(self.content)
