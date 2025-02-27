be9a9ef85f5d6e508f301e655949db6bd30b7a55
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2025-02-27 10:56:24 +0100

    separate info and note obj

--- src/pyslite/utils/note.py
+++ src/pyslite/utils/note.py
@@ -10,27 +10,30 @@ class ReviewState(Enum):
   Outdated = "Outdated"
   VerificationRequested = "VerificationRequested"
 
+class NoteInfo(BaseModel):
+  title: str
+  content: Optional[str] = None  # content can be missing
+  def show(self):
+        try:
+          from IPython import display, get_ipython
+          get_ipython()
+          if self.content:
+            display_content = f"#{self.title}\n" + self.content
+            display.display(display.Markdown(display_content))
+          
+        except:
+          print(self.title)
+          print(self.content)
 
-class Note(BaseModel):
-    updatedAt: datetime
-    url: str
-    parentNoteId: str
-    title: str
-    id: str
-    content: Optional[str] = None  # content can be missing
-    reviewState: Optional[ReviewState] = None
-    columns: Optional[List[str]] = None
-    attributes: Optional[List[str]] = None
+class Note(NoteInfo):
+  updatedAt: datetime
+  url: str
+  parentNoteId: str
+  id: str
+  reviewState: Optional[ReviewState] = None
+  columns: Optional[List[str]] = None
+  attributes: Optional[List[str]] = None
     
 
-    def show(self):
-      try:
-        from IPython import display, get_ipython
-        get_ipython()
-        if self.content:
-          display_content = f"#{self.title}\n" + self.content
-          display.display(display.Markdown(display_content))
-        
-      except:
-        print(self.title)
-        print(self.content)
+    
+
